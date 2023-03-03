89438fa9691e559a0db0d6cfab19af440358b74e
Kusuma Sandi <wijayakusumasandi@gmail.com>
2023-03-03 20:10:35 +0800

    Feature: adding logger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "../router";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
 routes(app);
 
